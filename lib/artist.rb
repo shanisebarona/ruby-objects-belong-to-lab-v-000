f748bfcd510248fa739d2b8ece55dc2cd6144f8e
Shanise Barona <shanisebarona@gmail.com>
2018-08-19 12:36:27 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :title, :artist 
+  
+end
+
+class Artist 
+  attr_accessor :name
+  
+end
+
+class Post 
+  attr_accessor :title, :author
+  
+end
+
+class Author 
+  attr_accessor :name
+  
+end
