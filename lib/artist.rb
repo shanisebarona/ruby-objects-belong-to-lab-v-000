2ba66634e4740fa0def439269a541338b01fa573
Shanise Barona <shanisebarona@gmail.com>
2018-08-29 12:11:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,11 @@
-class Song 
-  attr_accessor :title, :artist 
-  
-end
-
 class Artist 
   attr_accessor :name
   
 end
 
-class Post 
-  attr_accessor :title, :author
-  
-end
 
-class Author 
-  attr_accessor :name
-  
-end
+
+
+
+
+
